1a5d7bf1a85f573bef0a200be29c4f46a0b8d750
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-16 21:17:35 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -16,15 +16,18 @@ st.title("YouTube Video Downloader")
 # Input fields
 video_url = st.text_input("Enter the YouTube video URL", key="video_url")
 
+# Output directory selection
+output_directory = st.empty()
+
+# Choose location button
+if st.button("Choose Location"):
+    output_directory = st.file_uploader("Select output directory", type=None, accept_folders=True)
+
 # Download button
 if st.button("Download"):
-    if video_url:
-        output_directory = st.sidebar.text_input("Enter the output directory path", key="output_directory")
-        if output_directory:
-            st.text("Downloading...")
-            result = download_video(video_url, output_directory)
-            st.text(result)
-        else:
-            st.sidebar.warning("Please provide an output directory path.")
+    if video_url and output_directory:
+        st.text("Downloading...")
+        result = download_video(video_url, output_directory.name)
+        st.text(result)
     else:
-        st.warning("Please provide a video URL.")
+        st.warning("Please provide a video URL and select an output directory.")
