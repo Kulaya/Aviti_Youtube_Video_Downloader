69f86bba3ffd61c121479041e1235d200d3b0e8f
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-17 08:43:02 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,40 +1,40 @@
 import streamlit as st
-from pytube import YouTube
+import pytube
+import threading
 import os
+from tkinter import Tk, filedialog
 
-def download_video(url, output_path):
+def download_video(url, path):
     try:
-        yt = YouTube(url)
-        video = yt.streams.get_highest_resolution()  # Get the highest resolution stream
-        video.download(output_path)
-        return "Download completed successfully!"
+        st.write("Downloading...")
+        youtube = pytube.YouTube(url)
+        video = youtube.streams.get_highest_resolution()
+        video.download(path)
+        st.write("Download completed!")
     except Exception as e:
-        return f"An error occurred: {str(e)}"
+        st.write("An error occurred:", str(e))
 
-# Streamlit application
-st.title("YouTube Video Downloader")
+def get_download_location():
+    root = Tk()
+    root.withdraw()
+    folder_selected = filedialog.askdirectory()
+    root.destroy()
+    return folder_selected
 
-# Input fields
-video_url = st.text_input("Enter the YouTube video URL", key="video_url")
+def main():
+    st.title("YouTube Video Downloader")
 
-# Output directory selection
-output_directory = st.sidebar.empty()
-output_directory_path = None  # Initialize the output directory path variable
+    # Input URL
+    url = st.text_input("Enter YouTube Video URL", "")
 
-# Browse button
-if st.sidebar.button("Browse"):
-    output_directory_path = st.sidebar.file_uploader("Select output directory", type=None, accept_folders=True)
-    if output_directory_path is not None:
-        output_directory_path = os.path.abspath(output_directory_path.name)
-        output_directory.info(f"Output directory: {output_directory_path}")
-    else:
-        output_directory.warning("Please select an output directory.")
+    # Download button
+    if st.button("Download"):
+        # Get download location
+        path = get_download_location()
+        if path:
+            # Create a separate thread for downloading
+            download_thread = threading.Thread(target=download_video, args=(url, path))
+            download_thread.start()
 
-# Download button
-if st.button("Download"):
-    if video_url and output_directory_path:
-        st.text("Downloading...")
-        result = download_video(video_url, output_directory_path)
-        st.text(result)
-    else:
-        st.warning("Please provide a video URL and select an output directory.")
+if __name__ == '__main__':
+    main()
