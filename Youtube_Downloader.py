1b5cd5c85e3296b4332895a8b8c4b9c944f71061
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-17 09:23:38 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,36 +1,28 @@
-import streamlit as st
-import pytube
 import os
+import streamlit as st
+from pytube import YouTube
 
-def download_video(url, path):
+def download_video(url):
     try:
-        st.write("Downloading...")
-        youtube = pytube.YouTube(url)
-        video = youtube.streams.get_highest_resolution()
-        video.download(path)
-        st.write("Download completed!")
+        st.text('Downloading...')
+        yt = YouTube(url)
+        stream = yt.streams.get_highest_resolution()
+        stream.download()
+        st.success('Download completed!')
     except Exception as e:
-        st.write("An error occurred:", str(e))
+        st.error(f'Download failed: {str(e)}')
 
 def main():
-    st.title("YouTube Video Downloader")
-
-    # Input URL
-    url = st.text_input("Enter YouTube Video URL", "")
-
-    # Download location
-    st.write("Select download location:")
-    download_path = st.text_input("Folder path", "")
-
-    # Download button
-    if st.button("Download"):
-        if download_path:
-            # Show loading spinner
-            with st.spinner("Downloading..."):
-                # Download the video
-                download_video(url, download_path)
-                # Clear the spinner
-                st.spinner()
+    st.title('YouTube Video Downloader')
+    st.write('Enter the URL of the YouTube video you want to download:')
+    
+    url = st.text_input('URL')
+    
+    if st.button('Download'):
+        if url:
+            download_video(url)
+        else:
+            st.warning('Please enter a valid YouTube video URL.')
 
 if __name__ == '__main__':
     main()
