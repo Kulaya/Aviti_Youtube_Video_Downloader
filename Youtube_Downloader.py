194eed05961f80e4a6b80584164f03e9f434f7e6
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-17 09:28:01 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -7,8 +7,9 @@ def download_video(url):
         st.text('Downloading...')
         yt = YouTube(url)
         stream = yt.streams.get_highest_resolution()
-        stream.download()
+        file_path = stream.download()
         st.success('Download completed!')
+        return file_path
     except Exception as e:
         st.error(f'Download failed: {str(e)}')
 
@@ -20,9 +21,14 @@ def main():
     
     if st.button('Download'):
         if url:
-            download_video(url)
+            file_path = download_video(url)
+            if file_path:
+                st.success('Download completed!')
+                if st.button('Open Downloaded Video'):
+                    os.startfile(file_path)  # Open the file location
         else:
             st.warning('Please enter a valid YouTube video URL.')
 
 if __name__ == '__main__':
     main()
+
