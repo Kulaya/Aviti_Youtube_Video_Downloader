dee9da2098792b74a97d756cec1f377f010b5ce1
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-17 08:26:43 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pytube import YouTube
+import os
 
 def download_video(url, output_path):
     try:
@@ -17,15 +18,22 @@ st.title("YouTube Video Downloader")
 video_url = st.text_input("Enter the YouTube video URL", key="video_url")
 
 # Output directory selection
-output_directory = st.sidebar.text_input("Enter the output directory path", key="output_directory")
+output_directory = st.sidebar.empty()
 
-# Download checkbox
-download_checked = st.checkbox("Download")
+# Browse button
+if st.sidebar.button("Browse"):
+    output_directory_path = st.sidebar.file_uploader("Select output directory", type=None, key="output_directory")
+    if output_directory_path is not None:
+        output_directory_path = output_directory_path.name
+        output_directory.info(f"Output directory: {output_directory_path}")
+    else:
+        output_directory.warning("Please select an output directory.")
 
-if download_checked:
-    if video_url and output_directory:
+# Download button
+if st.button("Download"):
+    if video_url and output_directory_path:
         st.text("Downloading...")
-        result = download_video(video_url, output_directory)
+        result = download_video(video_url, output_directory_path)
         st.text(result)
     else:
-        st.warning("Please provide a video URL and output directory path.")
+        st.warning("Please provide a video URL and select an output directory.")
