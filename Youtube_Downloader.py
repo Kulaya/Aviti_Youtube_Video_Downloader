84e4dc0bbad61b680c8ec897967a9b260384c41c
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-18 22:14:12 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,10 +1,6 @@
 import streamlit as st
 from pytube import YouTube
-import webbrowser
 import os
-
-def open_video(video_path):
-    webbrowser.open(video_path)
 
 def download_video(url, download_path):
     st.write("Downloading...")
@@ -15,7 +11,15 @@ def download_video(url, download_path):
     st.write("Download completed!")
 
     # Open the downloaded video automatically
+    st.write("Opening video...")
     open_video(video_path)
+
+def open_video(video_path):
+    if os.name == "nt":  # Windows
+        os.startfile(video_path)
+    else:
+        opener = "open" if os.name == "posix" else "xdg-open"
+        os.system(f"{opener} '{video_path}'")
 
 st.title("YouTube Video Downloader")
 
@@ -23,20 +27,5 @@ st.title("YouTube Video Downloader")
 url = st.text_input("Enter the YouTube video URL:")
 if url:
     if st.button("Download"):
-        # Get the list of directories in the user's home directory
-        directory_list = os.listdir(path=os.path.expanduser("~"))
-
-        # Prepend "Downloads" to the directory list
-        directory_list.insert(0, "Downloads")
-
-        # Select the download location using a file dialog
-        download_path = st.sidebar.selectbox("Select download location", directory_list)
-        if download_path:
-            if download_path == "Downloads":
-                download_path = os.path.join(os.path.expanduser("~"), "Downloads")
-            else:
-                download_path = os.path.join(os.path.expanduser("~"), download_path)
-
-            download_video(url, download_path)
-        else:
-            st.warning("No download location selected.")
+        download_path = os.path.join(os.path.expanduser("~"), "Downloads")
+        download_video(url, download_path)
