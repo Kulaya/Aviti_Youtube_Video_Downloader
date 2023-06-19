af20fbf556b9b89766aa4e408143886fbbda2b04
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-20 00:17:51 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -12,8 +12,9 @@ def download_video(url, download_path):
         st.text("Downloading video...")
         yt = YouTube(url)
         stream = yt.streams.get_highest_resolution()
-        download_path = os.path.join(download_path, stream.default_filename)
-        stream.download(output_path=download_path)
+        download_folder = os.path.join(download_path, yt.title)
+        os.makedirs(download_folder, exist_ok=True)
+        stream.download(output_path=download_folder)
         st.success("Video downloaded successfully!")
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
@@ -22,11 +23,11 @@ def download_video(url, download_path):
 video_url = st.sidebar.text_input("Enter YouTube video URL")
 
 # Download path selection
-download_path = st.sidebar.file_uploader("Select download path", type=None)
+download_path = st.sidebar.folder_selector("Select download folder")
 
 # Download button
 if st.sidebar.button("Download"):
     if video_url and download_path:
-        download_video(video_url, download_path.name)
+        download_video(video_url, download_path)
     else:
-        st.warning("Please enter a valid YouTube video URL and select a download path.")
+        st.warning("Please enter a valid YouTube video URL and select a download folder.")
