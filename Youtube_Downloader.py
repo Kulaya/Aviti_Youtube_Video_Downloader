a2189d029f37abc671b213aa4689dd222c00564a
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-06-20 15:02:26 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,39 +1,39 @@
 import streamlit as st
 from pytube import YouTube
-import os
-import tempfile
 
-# Set Streamlit app title and layout
-st.title("YouTube Video Downloader")
-st.sidebar.header("Settings")
+# Set page title
+st.set_page_config(page_title="YouTube Downloader")
 
-# Function to download YouTube video
-def download_video(url, download_path):
+# Define function to download YouTube video
+def download_video(video_url, video_format):
     try:
-        st.text("Downloading video...")
-        yt = YouTube(url)
-        stream = yt.streams.get_highest_resolution()
-        download_folder = os.path.join(download_path, yt.title)
-        os.makedirs(download_folder, exist_ok=True)
-        stream.download(output_path=download_folder)
+        # Create YouTube object
+        yt = YouTube(video_url)
+
+        # Get video stream with selected format
+        stream = yt.streams.get_by_resolution(video_format)
+
+        # Download video
+        stream.download()
+
+        # Display success message
         st.success("Video downloaded successfully!")
     except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+        # Display error message
+        st.error("An error occurred: " + str(e))
 
-# User input for YouTube video URL
-video_url = st.sidebar.text_input("Enter YouTube video URL")
+# Page title
+st.title("YouTube Downloader")
 
-# File uploader for selecting the download folder path
-folder_path_file = st.sidebar.file_uploader(
-    "Upload a file containing the download folder path",
-    type=["txt"],
-    accept_multiple_files=False
-)
+# Video URL input
+video_url = st.text_input("Enter the URL of the YouTube video you want to download:")
+
+# Video format selection
+video_format = st.selectbox("Select the video format you want to download:", ["720p", "480p", "360p"])
 
 # Download button
-if st.sidebar.button("Download"):
-    if video_url and folder_path_file is not None:
-        folder_path = folder_path_file.getvalue().decode("utf-8").strip()
-        download_video(video_url, folder_path)
+if st.button("Download"):
+    if not video_url:
+        st.warning("Please enter a valid YouTube video URL.")
     else:
-        st.warning("Please enter a valid YouTube video URL and select a download folder path.")
+        download_video(video_url, video_format)
