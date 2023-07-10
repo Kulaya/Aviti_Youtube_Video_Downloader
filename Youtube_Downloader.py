82f56efd859b0b9610e829f1bffff1547ec38b27
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-07-10 12:24:43 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,20 +1,42 @@
-def main():
-    st.title("YouTube Video Downloader")
-    video_url = st.text_input("Enter the YouTube video URL:")
-    download_button = st.button("Download")
+import youtube_dl
+import streamlit as st
 
-    if download_button:
-           if video_url:
-               try:
-                   st.write("Downloading...")
-                   yt = YouTube(video_url)
-                   stream = yt.streams.get_highest_resolution()
-                   stream.download()
-                   st.success("Video downloaded successfully!")
-               except Exception as e:
-                   st.error(f"An error occurred: {str(e)}")
-           else:
-               st.warning("Please enter a valid YouTube video URL.")
+def download_video(url, format, quality):
+    try:
+        # Set the options for downloading the video
+        options = {
+            'format': format,
+            'outtmpl': '~/Downloads/%(title)s.%(ext)s',
+            'quiet': True,
+        }
 
-    if __name__ == "__main__":
-       main()
+        # Add the requested quality if specified
+        if quality:
+            options['format'] += f'+{quality}'
+
+        # Create a YouTubeDL object
+        ydl = youtube_dl.YoutubeDL(options)
+
+        # Download the video
+        with ydl:
+            result = ydl.extract_info(url, download=True)
+
+        st.success("Video downloaded successfully!")
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
+
+# Streamlit application
+st.title("YouTube Video Downloader")
+
+# Input fields
+video_url = st.text_input("Enter YouTube video URL")
+format = st.selectbox("Select format", ['bestvideo+bestaudio/best', 'bestvideo', 'bestaudio'])
+quality = st.selectbox("Select quality", ['1080p', '720p', '480p', '360p', '240p', '144p'])
+download_button = st.button("Download")
+
+# Perform download when Download button is clicked
+if download_button:
+    if video_url:
+        download_video(video_url, format, quality)
+    else:
+        st.warning("Please enter a YouTube video URL.")
