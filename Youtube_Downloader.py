b4abf6feca3996f72c93a49a59016a0f53c59361
Kulaya <106235512+Kulaya@users.noreply.github.com>
2023-07-11 13:53:14 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -6,6 +6,11 @@ from pathlib import Path
 # Set the Downloads folder path
 DOWNLOADS_PATH = Path.home() / "Downloads"
 
+# Extract video ID from YouTube URL
+def extract_video_id(url):
+    video_id = pytube.extract.video_id(url)
+    return video_id
+
 # Streamlit app
 def main():
     st.title("YouTube Video Downloader")
@@ -15,20 +20,26 @@ def main():
 
     if st.button("Download"):
         if video_url:
-            # Download the video
-            try:
-                st.text("Downloading...")
-                yt = pytube.YouTube(video_url)
-                stream = yt.streams.first()
-                file_path = DOWNLOADS_PATH / stream.default_filename
-                stream.download(output_path=str(DOWNLOADS_PATH))
+            # Extract video ID from URL
+            video_id = extract_video_id(video_url)
 
-                st.success("Download complete!")
-                st.text(f"Video saved to: {file_path}")
-            except Exception as e:
-                st.error(f"An error occurred: {str(e)}")
+            if video_id:
+                # Download the video
+                try:
+                    st.text("Downloading...")
+                    yt = pytube.YouTube(video_id)
+                    stream = yt.streams.first()
+                    file_path = DOWNLOADS_PATH / stream.default_filename
+                    stream.download(output_path=str(DOWNLOADS_PATH))
+
+                    st.success("Download complete!")
+                    st.text(f"Video saved to: {file_path}")
+                except Exception as e:
+                    st.error(f"An error occurred: {str(e)}")
+            else:
+                st.warning("Please enter a valid YouTube video URL.")
         else:
-            st.warning("Please enter a valid YouTube video URL.")
+            st.warning("Please enter a YouTube video URL.")
 
 # Run the Streamlit app
 if __name__ == "__main__":
