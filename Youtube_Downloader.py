d2e6ed98a47de2449451892245f399743dd054f0
Kulaya <106235512+Kulaya@users.noreply.github.com>
2025-01-08 15:07:17 +0300

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -2,7 +2,10 @@ import streamlit as st
 import yt_dlp
 
 def download_video(url):
-    ydl_opts = {}
+    # Specify a custom save directory
+    ydl_opts = {
+        'outtmpl': 'downloads/%(title)s.%(ext)s',  # Saves to 'downloads' folder with video title as filename
+    }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
